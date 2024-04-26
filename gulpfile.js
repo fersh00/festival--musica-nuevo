4d3543d1ad2409949fa9f37907b4438779dfb4ce
fersh00 <fc323897@gmail.com>
2024-04-26 12:25:20 -0700

    agregando gulp-sass

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+// export function hola(done) {
+//     console.log('Hola desdde gulpfile.js');
+//     done()
+// }
+
+const sass = gulpSass(dartSass)
